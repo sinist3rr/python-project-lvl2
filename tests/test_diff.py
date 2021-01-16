1d16e8f700ae7c93fd0b6fdd89e96a54dc6e50f4
sinist3rr <darkfloki@protonmail.com>
2021-01-16 13:04:25 +0200

    fix spaces

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -7,48 +7,56 @@ from gendiff import generate_diff
 
 
 @pytest.mark.parametrize(
-     'file_before,file_after,format,result',
-     [(
-             'tests/fixtures/before.json',
-             'tests/fixtures/after.json',
-             'stylish',
-             'sample_result'
-     ), (
-             'tests/fixtures/before2.json',
-             'tests/fixtures/before2.json',
-             'stylish',
-             'sample_result2'
-     ), (
-             'tests/fixtures/before.yml',
-             'tests/fixtures/after.yml',
-             'stylish',
-             'sample_result'
-     ), (
-             'tests/fixtures/before2.yml',
-             'tests/fixtures/before2.yml',
-             'stylish',
-             'sample_result2'
-     ), (
-             'tests/fixtures/before_nested.json',
-             'tests/fixtures/after_nested.json',
-             'stylish',
-             'sample_nested_result'
-     ), (
-             'tests/fixtures/before_nested.yml',
-             'tests/fixtures/after_nested.yml',
-             'stylish',
-             'sample_nested_result'
-     ), (
-             'tests/fixtures/before_nested.json',
-             'tests/fixtures/after_nested.json',
-             'plain',
-             'sample_nested_result_plain'
-     ), (
-             'tests/fixtures/before_nested.json',
-             'tests/fixtures/after_nested.json',
-             'json',
-             'sample_nested_result_json'
-     )]
+    'file_before,file_after,format,result',
+    [
+        (
+            'tests/fixtures/before.json',
+            'tests/fixtures/after.json',
+            'stylish',
+            'sample_result'
+        ),
+        (
+            'tests/fixtures/before2.json',
+            'tests/fixtures/before2.json',
+            'stylish',
+            'sample_result2'
+        ),
+        (
+            'tests/fixtures/before.yml',
+            'tests/fixtures/after.yml',
+            'stylish',
+            'sample_result'
+        ),
+        (
+            'tests/fixtures/before2.yml',
+            'tests/fixtures/before2.yml',
+            'stylish',
+            'sample_result2'
+        ),
+        (
+            'tests/fixtures/before_nested.json',
+            'tests/fixtures/after_nested.json',
+            'stylish',
+            'sample_nested_result'
+        ),
+        (
+            'tests/fixtures/before_nested.yml',
+            'tests/fixtures/after_nested.yml',
+            'stylish',
+            'sample_nested_result'
+        ),
+        (
+            'tests/fixtures/before_nested.json',
+            'tests/fixtures/after_nested.json',
+            'plain',
+            'sample_nested_result_plain'
+        ),
+        (
+            'tests/fixtures/before_nested.json',
+            'tests/fixtures/after_nested.json',
+            'json',
+            'sample_nested_result_json'
+        )]
 )
 def test_gendiff(file_before, file_after, format, result):
     filepath = 'tests/fixtures/{}.txt'.format(result)

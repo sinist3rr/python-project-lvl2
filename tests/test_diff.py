4712011a1bf18e044e33038562dacf929b5afd9b
sinist3rr <darkfloki@protonmail.com>
2021-01-16 12:50:11 +0200

    use parametrize

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -1,96 +1,58 @@
 # -*- coding:utf-8 -*-
 
-"""Simple json diff tests."""
+"""Various diff tests."""
 
 import pytest
 from gendiff import generate_diff
 
 
-@pytest.fixture
-def sample_paths_json():
-    return ('tests/fixtures/before.json', 'tests/fixtures/after.json', 'tests/fixtures/before2.json', 'tests/fixtures/before_nested.json', 'tests/fixtures/after_nested.json')
-
-
-@pytest.fixture
-def sample_paths_yaml():
-    return ('tests/fixtures/before.yml', 'tests/fixtures/after.yml', 'tests/fixtures/before2.yml', 'tests/fixtures/before_nested.yml', 'tests/fixtures/after_nested.yml')
-
-
-@pytest.fixture
-def sample_result():
-    filepath = 'tests/fixtures/sample_result1.txt'
+@pytest.mark.parametrize(
+     'file_before,file_after,format,result',
+     [(
+             'tests/fixtures/before.json',
+             'tests/fixtures/after.json',
+             'stylish',
+             'sample_result'
+     ), (
+             'tests/fixtures/before2.json',
+             'tests/fixtures/before2.json',
+             'stylish',
+             'sample_result2'
+     ), (
+             'tests/fixtures/before.yml',
+             'tests/fixtures/after.yml',
+             'stylish',
+             'sample_result'
+     ), (
+             'tests/fixtures/before2.yml',
+             'tests/fixtures/before2.yml',
+             'stylish',
+             'sample_result2'
+     ), (
+             'tests/fixtures/before_nested.json',
+             'tests/fixtures/after_nested.json',
+             'stylish',
+             'sample_nested_result'
+     ), (
+             'tests/fixtures/before_nested.yml',
+             'tests/fixtures/after_nested.yml',
+             'stylish',
+             'sample_nested_result'
+     ), (
+             'tests/fixtures/before_nested.json',
+             'tests/fixtures/after_nested.json',
+             'plain',
+             'sample_nested_result_plain'
+     ), (
+             'tests/fixtures/before_nested.json',
+             'tests/fixtures/after_nested.json',
+             'json',
+             'sample_nested_result_json'
+     )]
+)
+def test_gendiff(file_before, file_after, format, result):
+    filepath = 'tests/fixtures/{}.txt'.format(result)
     with open(filepath) as fp:
-        line = fp.read()
-        return line
+        result_line = fp.read()
 
-
-@pytest.fixture
-def sample_result2():
-    filepath = 'tests/fixtures/sample_result2.txt'
-    with open(filepath) as fp:
-        line = fp.read()
-        return line
-
-
-@pytest.fixture
-def sample_nested_result():
-    filepath = 'tests/fixtures/sample_nested_result.txt'
-    with open(filepath) as fp:
-        line = fp.read()
-        return line
-
-
-@pytest.fixture
-def sample_nested_result_plain():
-    filepath = 'tests/fixtures/sample_nested_result_plain.txt'
-    with open(filepath) as fp:
-        line = fp.read()
-        return line
-
-
-@pytest.fixture
-def sample_nested_result_json():
-    filepath = 'tests/fixtures/sample_nested_result_json.txt'
-    with open(filepath) as fp:
-        line = fp.read()
-        return line
-
-
-def test_simple_json(sample_paths_json, sample_result):
-    """Check that json diff works."""
-    assert generate_diff(sample_paths_json[0], sample_paths_json[1], 'stylish') == sample_result
-
-
-def test_simple_json2(sample_paths_json, sample_result2):
-    """Check that json diff works."""
-    assert generate_diff(sample_paths_json[2], sample_paths_json[2], 'stylish') == sample_result2
-
-
-def test_simple_yaml(sample_paths_yaml, sample_result):
-    """Check that yaml diff works."""
-    assert generate_diff(sample_paths_yaml[0], sample_paths_yaml[1], 'stylish') == sample_result
-
-
-def test_simple_yaml2(sample_paths_yaml, sample_result2):
-    """Check that yaml diff works."""
-    assert generate_diff(sample_paths_yaml[2], sample_paths_yaml[2], 'stylish') == sample_result2
-
-
-def test_nested_json(sample_paths_json, sample_nested_result):
-    """Check that json diff works."""
-    assert generate_diff(sample_paths_json[3], sample_paths_json[4], 'stylish') == sample_nested_result
-
-
-def test_nested_yaml(sample_paths_yaml, sample_nested_result):
-    """Check that yaml diff works."""
-    assert generate_diff(sample_paths_yaml[3], sample_paths_yaml[4], 'stylish') == sample_nested_result
-
-
-def test_nested_json_plain(sample_paths_json, sample_nested_result_plain):
-    """Check that json diff works in plain format."""
-    assert generate_diff(sample_paths_json[3], sample_paths_json[4], 'plain') == sample_nested_result_plain
-
-
-def test_nested_json_json(sample_paths_json, sample_nested_result_json):
-    """Check that json diff works in valid json format."""
-    assert generate_diff(sample_paths_json[3], sample_paths_json[4], 'json') == sample_nested_result_json
+    assert generate_diff(file_before, file_after, format) == result_line

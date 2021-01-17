b840ef60d9c5a2543f768d70e4866d61e3aa69ba
sinist3rr <darkfloki@protonmail.com>
2021-01-17 12:15:21 +0200

    fix exceptions

--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -33,13 +33,7 @@ def diff_tree(d1, d2):
 def generate_diff(file1, file2, format='stylish'):
     file1_dict = parsing_file(file1)
     file2_dict = parsing_file(file2)
-
-    try:
-        tree = diff_tree(file1_dict, file2_dict)
-    except AttributeError:
-        print('Cannot make diff tree.')
-        print('File is not found or empty.')
-        return
+    tree = diff_tree(file1_dict, file2_dict)
 
     if format == 'stylish':
         final_result = '{}}}'.format(stylish_format(tree))
--- gendiff/parse_files.py
+++ gendiff/parse_files.py
@@ -1,11 +1,18 @@
 import json
 import yaml
+from yaml.scanner import ScannerError
+from json import JSONDecodeError
 import os
+import sys
 
 
 def parsing_file(filename):
     JSON_EXTENSIONS = ['.json', '.JSON']
     YAML_EXTENSIONS = ['.yml', '.yaml', '.YAML', '.YML']
+
+    if not os.path.exists(filename):
+        print('File not found')
+        sys.exit(1)
 
     _, ext = os.path.splitext(filename)
     try:
@@ -15,5 +22,6 @@ def parsing_file(filename):
         if ext in YAML_EXTENSIONS:
             with open(filename) as ymlfile:
                 return yaml.load(ymlfile, Loader=yaml.FullLoader)
-    except json.JSONDecodeError:
+    except (JSONDecodeError, ScannerError):
         print("Invalid file type.")
+        sys.exit(1)

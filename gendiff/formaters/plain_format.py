b3d81d8b90f42deb40c3b946c970229ce79af1d8
sinist3rr <darkfloki@protonmail.com>
2021-01-18 18:08:13 +0200

    optimize conditions

--- gendiff/formaters/plain_format.py
+++ gendiff/formaters/plain_format.py
@@ -34,32 +34,18 @@ def plain_format(diff_tree, result='', name=''):
                 )
             )
         elif is_added(node):
-            if not name:
-                result += "Property '{}' was added with value: {}\n".format(
-                    node['name'], is_complex(node['value'])
-                )
-            else:
-                result += "Property '{}' was added with value: {}\n".format(
-                    name + '.' + node['name'], is_complex(node['value'])
-                )
+            result += "Property '{f1}{f2}' was added with value: {f3}\n".format(
+                f1='' if not name else name + '.', f2=node['name'], f3=is_complex(node['value'])
+            )
         elif is_deleted(node):
-            if not name:
-                result += "Property '{}' was removed\n".format(node['name'])
-            else:
-                result += "Property '{}' was removed\n".format(
-                    name + '.' + node['name']
-                )
+            result += "Property '{f1}{f2}' was removed\n".format(
+                f1='' if not name else name + '.', f2=node['name']
+            )
         elif is_changed(node):
-            if not name:
-                result += "Property '{}' was updated. From {} to {}\n".format(
-                    node['name'],
-                    is_complex(node['value'][0]),
-                    is_complex(node['value'][1])
-                )
-            else:
-                result += "Property '{}' was updated. From {} to {}\n".format(
-                    name + '.' + node['name'],
-                    is_complex(node['value'][0]),
-                    is_complex(node['value'][1])
-                )
+            result += "Property '{f1}{f2}' was updated. From {f3} to {f4}\n".format(
+                f1='' if not name else name + '.',
+                f2=node['name'],
+                f3=is_complex(node['value'][0]),
+                f4=is_complex(node['value'][1])
+            )
     return result

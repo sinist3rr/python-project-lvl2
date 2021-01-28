b7785231b47cc995f9d6100c656a188d057e0c41
sinist3rr <darkfloki@protonmail.com>
2021-01-28 17:40:28 +0200

    simplify a bit

--- gendiff/formaters/plain_format.py
+++ gendiff/formaters/plain_format.py
@@ -24,27 +24,30 @@ def is_complex(value):
 
 def plain_format(diff_tree, result='', name=''):
     diff_tree.sort(key=lambda x: x['name'])
+    if not name:
+        f1 = ''
+    else:
+        f1 = name + '.'
     for node in diff_tree:
+        f2 = node['name']
         if is_nested(node):
             result = plain_format(
                 get_children(node),
                 result,
-                "{f1}{f2}".format(
-                    f1="" if not name else name + ".", f2=node['name']
-                )
+                "{}{}".format(f1, f2)
             )
         elif is_added(node):
-            result += "Property '{f1}{f2}' was added with value: {f3}\n".format(
-                f1='' if not name else name + '.', f2=node['name'], f3=is_complex(node['value'])
+            result += "Property '{}{}' was added with value: {f3}\n".format(
+                f1, f2, f3=is_complex(node['value'])
             )
         elif is_deleted(node):
-            result += "Property '{f1}{f2}' was removed\n".format(
-                f1='' if not name else name + '.', f2=node['name']
+            result += "Property '{}{}' was removed\n".format(
+                f1, f2
             )
         elif is_changed(node):
-            result += "Property '{f1}{f2}' was updated. From {f3} to {f4}\n".format(
-                f1='' if not name else name + '.',
-                f2=node['name'],
+            result += "Property '{}{}' was updated. From {f3} to {f4}\n".format(
+                f1,
+                f2,
                 f3=is_complex(node['value'][0]),
                 f4=is_complex(node['value'][1])
             )

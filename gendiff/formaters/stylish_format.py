2aacd7da87b1fa11572b9ef781e1dd4984bdfaa5
sinist3rr <darkfloki@protonmail.com>
2021-01-28 16:28:43 +0200

    add sign vars

--- gendiff/formaters/stylish_format.py
+++ gendiff/formaters/stylish_format.py
@@ -13,6 +13,9 @@ START_SPACES = 2
 OPEN_BLOCK = '{}{} {}: {{\n'
 VALUE_BLOCK = '{}{} {}: {}\n'
 CLOSE_BLOCK = '{}  }}\n'
+ADDED = '+'
+REMOVED = '-'
+STALE = ' '
 
 
 def stylish_format(diff_tree, spaces=START_SPACES, result='{\n'):
@@ -26,14 +29,14 @@ def stylish_format(diff_tree, spaces=START_SPACES, result='{\n'):
             result = stylish_format(get_children(node), spaces + REGULAR_SPACES, result)
             result += CLOSE_BLOCK.format(mult_space(spaces))
         elif is_added(node):
-            result = set_sign(result, spaces, '+', name, value)
+            result = set_sign(result, spaces, ADDED, name, value)
         elif is_deleted(node):
-            result = set_sign(result, spaces, '-', name, value)
+            result = set_sign(result, spaces, REMOVED, name, value)
         elif is_unchanged(node):
-            result = set_sign(result, spaces, ' ', name, value)
+            result = set_sign(result, spaces, STALE, name, value)
         elif is_changed(node):
-            result = set_sign(result, spaces, '-', name, value[0])
-            result = set_sign(result, spaces, '+', name, value[1])
+            result = set_sign(result, spaces, REMOVED, name, value[0])
+            result = set_sign(result, spaces, ADDED, name, value[1])
     return result
 
 

0327a108cf01476ac9d6d476ea6dfebacb91edf8
sinist3rr <darkfloki@protonmail.com>
2021-01-29 15:47:54 +0200

    remove concat

--- gendiff/formaters/plain_format.py
+++ gendiff/formaters/plain_format.py
@@ -8,38 +8,38 @@ from gendiff.diff_structure import (
 
 
 def plain_format_wrapper(diff_tree):
-    return plain_format(diff_tree)[:-1]
+    return ''.join(plain_format(diff_tree))[:-1]
 
 
-def plain_format(diff_tree, result='', name=''):
+def plain_format(diff_tree, result=[], name=''):
     diff_tree.sort(key=lambda x: x['name'])
     if not name:
-        f1 = ''
+        composite_key = ''
     else:
-        f1 = name + '.'
+        composite_key = name + '.'
     for node in diff_tree:
-        f2 = node['name']
+        key = node['name']
         if is_nested(node):
             result = plain_format(
                 get_children(node),
                 result,
-                "{}{}".format(f1, f2)
+                "{}{}".format(composite_key, key)
             )
         elif is_added(node):
-            result += "Property '{}{}' was added with value: {f3}\n".format(
-                f1, f2, f3=is_complex(node['value'])
-            )
+            result.append("Property '{}{}' was added with value: {}\n".format(
+                composite_key, key, is_complex(node['value'])
+            ))
         elif is_deleted(node):
-            result += "Property '{}{}' was removed\n".format(
-                f1, f2
-            )
+            result.append("Property '{}{}' was removed\n".format(
+                composite_key, key
+            ))
         elif is_changed(node):
-            result += "Property '{}{}' was updated. From {f3} to {f4}\n".format(
-                f1,
-                f2,
-                f3=is_complex(node['value'][0]),
-                f4=is_complex(node['value'][1])
-            )
+            result.append("Property '{}{}' was updated. From {} to {}\n".format(
+                composite_key,
+                key,
+                is_complex(node['value'][0]),
+                is_complex(node['value'][1])
+            ))
     return result
 
 

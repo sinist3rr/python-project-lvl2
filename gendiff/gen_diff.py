6fdd7fee2cc4bd850f9b1e381ee445132d5e4702
sinist3rr <darkfloki@protonmail.com>
2021-02-02 15:16:13 +0200

    create styles consts

--- gendiff/gen_diff.py
+++ gendiff/gen_diff.py
@@ -1,9 +1,9 @@
 from gendiff.parse_files import parsing_file
-from gendiff.formaters.helpers import select_formater
+from gendiff.formaters.helpers import select_formater, STYLISH_STYLE
 from gendiff.diff_structure import diff_tree
 
 
-def generate_diff(file1, file2, format='stylish'):
+def generate_diff(file1, file2, format=STYLISH_STYLE):
     file1_dict = parsing_file(file1)
     file2_dict = parsing_file(file2)
     tree = diff_tree(file1_dict, file2_dict)

af72fe75e7a639f713571c203b95286ac32f67af
sinist3rr <darkfloki@protonmail.com>
2021-02-02 15:14:04 +0200

    create styles consts

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,4 +1,5 @@
 import argparse
+from gendiff.formaters.helpers import STYLISH_STYLE, PLAIN_STYLE, JSON_STYLE
 
 
 def prompt_args():
@@ -6,6 +7,6 @@ def prompt_args():
     parser.add_argument(dest='first_file')
     parser.add_argument(dest='second_file')
     parser.add_argument('-f', '--format',
-                        dest='FORMAT', default='stylish',
-                        choices=['stylish', 'plain', 'json'], help="set format of output")
+                        dest='FORMAT', default=STYLISH_STYLE,
+                        choices=[STYLISH_STYLE, PLAIN_STYLE, JSON_STYLE], help="set format of output")
     return parser
--- gendiff/formaters/helpers.py
+++ gendiff/formaters/helpers.py
@@ -3,10 +3,15 @@ from gendiff.formaters.plain_format import plain_format_wrapper
 from gendiff.formaters.json_format import json_format
 
 
+STYLISH_STYLE = 'stylish'
+PLAIN_STYLE = 'plain'
+JSON_STYLE = 'json'
+
+
 def select_formater(format, tree):
-    if format == 'stylish':
+    if format == STYLISH_STYLE:
         return stylish_format_wrapper(tree)
-    elif format == 'plain':
+    elif format == PLAIN_STYLE:
         return plain_format_wrapper(tree)
-    elif format == 'json':
+    elif format == JSON_STYLE:
         return json_format(tree)

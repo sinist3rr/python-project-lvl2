269e0b9622550a6bdff31dd8e09b4625f642cbfd
sinist3rr <darkfloki@protonmail.com>
2021-02-03 16:02:27 +0200

    rename func

--- gendiff/formaters/json.py
+++ gendiff/formaters/json.py
@@ -6,14 +6,14 @@ from gendiff.diff_structure import (
 )
 
 
-def json_prepare(diff_tree):
+def sort_diff_tree(diff_tree):
     diff_tree.sort(key=lambda x: x['name'])
     for node in diff_tree:
         if is_nested(node):
-            json_prepare(get_children(node))
+            sort_diff_tree(get_children(node))
     return diff_tree
 
 
 def json_format(diff_tree):
     new_structure = copy.deepcopy(diff_tree)
-    return json.dumps(json_prepare(new_structure))
+    return json.dumps(sort_diff_tree(new_structure))

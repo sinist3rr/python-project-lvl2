21400b06903c35351f916218d97d605986f7c6f1
sinist3rr <darkfloki@protonmail.com>
2021-02-05 14:50:47 +0200

    fix and optimize

--- gendiff/file_parser.py
+++ gendiff/file_parser.py
@@ -5,14 +5,21 @@ from json import JSONDecodeError
 import os
 
 
+JSON_EXT = '.json'
+YAML_EXT = ('.yml', '.yaml')
+
+
 def parse_file(filename):
     _, ext = os.path.splitext(filename)
+    lowercase_extension = ext.lower()
     try:
         with open(filename, 'r') as file:
-            if ext.lower() == '.json':
+            if lowercase_extension == JSON_EXT:
                 return json.load(file)
-            if ext.lower() in ('.yml', '.yaml'):
+            elif lowercase_extension in YAML_EXT:
                 return yaml.load(file, Loader=yaml.FullLoader)
+            else:
+                print('Incorrect extension {}'.format(ext))
     except (JSONDecodeError, ScannerError):
         raise ValueError("Invalid file type.")
     except IOError:

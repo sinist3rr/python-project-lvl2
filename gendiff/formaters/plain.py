0b837811809db84790e64a203b29c38d9b09bcdc
sinist3rr <darkfloki@protonmail.com>
2021-02-05 20:05:06 +0200

    optimized

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -8,35 +8,32 @@ from gendiff.diff_structure import (
 
 
 def plain_format(diff_tree):
-    return ''.join(_plain_format(diff_tree, []))[:-1]
+    return ''.join(_plain_format(diff_tree, [], []))[:-1]
 
 
-def _plain_format(diff_tree, result, name=''):
+def _plain_format(diff_tree, result, composite_key):
     sorted_diff_tree = sorted(diff_tree, key=lambda x: x['name'])
-    if not name:
-        composite_key = ''
-    else:
-        composite_key = name + '.'
     for node in sorted_diff_tree:
         key = node['name']
+        mixed_key = [*composite_key, key]
+        formatted_mixed_key = '.'.join(mixed_key)
         if is_nested(node):
             result = _plain_format(
                 get_children(node),
                 result,
-                "{}{}".format(composite_key, key)
+                mixed_key
             )
         elif is_added(node):
-            result.append("Property '{}{}' was added with value: {}\n".format(
-                composite_key, key, format_plain_value(node['value'])
+            result.append("Property '{}' was added with value: {}\n".format(
+                formatted_mixed_key, format_plain_value(node['value'])
             ))
         elif is_deleted(node):
-            result.append("Property '{}{}' was removed\n".format(
-                composite_key, key
+            result.append("Property '{}' was removed\n".format(
+                formatted_mixed_key
             ))
         elif is_changed(node):
-            result.append("Property '{}{}' was updated. From {} to {}\n".format(
-                composite_key,
-                key,
+            result.append("Property '{}' was updated. From {} to {}\n".format(
+                formatted_mixed_key,
                 format_plain_value(node['value'][0]),
                 format_plain_value(node['value'][1])
             ))
